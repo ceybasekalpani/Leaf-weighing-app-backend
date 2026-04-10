95d02031e48a186f85344503cafe1e79a4f09a85
ceybasekalpani <ceybasekalpani@gmail.com>
2026-04-10 17:58:03 +0530

    Fix some issues✏️

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'leaf-type']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
